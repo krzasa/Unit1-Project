f97d90c679548f7ba4c7ee50517b9aa23fcc911c
krzasa <krzasa96@gmail.com>
2024-04-10 14:21:50 -0400

    Made a folder for cookie game

---
+++ CookieGame/app.js
@@ -0,0 +1,7 @@
+let viewerElement = document.querySelector(".viewContainer");
+
+let direcElement = document.querySelector('.messageCon')
+
+const rewardButton = document.querySelector('.rewardButt')
+
+
